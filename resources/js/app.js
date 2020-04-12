d4a4a1246e42a4205e2c862291b3d2548a249da3
Emon04 <emonahmedea38@gmail.com>
2020-04-12 23:41:23 +0600

    Completed with CRUD and Instant Search, Reload without refresh page etc.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,23 +5,20 @@
  */
 
 require('./bootstrap');
-
-import Vue from 'vue'
+import Vue from 'vue';
+import { Form, HasError, AlertError } from 'vform'
 import VueProgressBar from 'vue-progressbar'
-import Vue from 'vue'
 import Snotify, { SnotifyPosition } from 'vue-snotify'
-
+window.Form = Form
 const SnotifyOptions = {
     toast: {
         position: SnotifyPosition.rightTop
     }
 }
-
 Vue.use(Snotify, SnotifyOptions)
-
 const VueProgressBarOptions = {
     color: '#50d38a',
-    failedColor: '#874b4b',
+    failedColor: '#87111d',
     thickness: '5px',
     transition: {
         speed: '0.2s',
@@ -31,30 +28,14 @@ const VueProgressBarOptions = {
     autoRevert: true,
     location: 'top',
     inverse: false
-}
+};
 Vue.use(VueProgressBar, VueProgressBarOptions);
 
+Vue.component('customer-component', require('./components/CustomerComponent.vue'));
+Vue.component('pagination', require('./components/partial/PaginationComponent.vue'));
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/CustomerComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('customer-component', require('./components/CustomerComponent.vue').default);
-Vue.component('pagination', require('./components/partial/PaginationComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
 const app = new Vue({
-    el: '#app',
+    el: '#app'
 });
